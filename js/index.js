aadbd2bf21180217c05a0585278290b87dbc678b
Saliou1920 <diopsaliou45@gmail.com>
2021-05-17 16:58:46 -0300

    Adding event listener

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 // event listener
-document.querySelector("button").addEventListener("click", handleClick);
+let listener = [];
+listener = document.querySelectorAll("button");
+for (let index = 0; index < listener.length; index++) {
+    listener[index].addEventListener("click", handleClick);
+}
+
+
 function handleClick() {
     alert("i got click");
 }
